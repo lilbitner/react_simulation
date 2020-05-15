6bf487402a94551247f65caa7bfad1c672e8db95
github username <github email address>
2020-05-15 16:10:07 -0600

    Add pathname functionality

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Link, Redirect, NavLink} from 'react-router-dom'
 import Datetime from 'react-datetime'
+import '../Styling/Home.css'
 
 
 
@@ -17,6 +18,8 @@ export default class Home extends React.Component{
         }, 1000)
     }
 
+   
+
     // logout = () => {
     //         localStorage.removeItem('token')
     //         this.props.history.push('/login')
@@ -26,11 +29,11 @@ export default class Home extends React.Component{
     render() {
         return(
             <div className='centerContainer'> 
-                <h2 id='welcomeName'>Welcome to RiskManagement {this.props.user.username}</h2>
-                <h2>Current Time</h2>
-                <h2>{this.state.datetime.toLocaleTimeString()}</h2>
-                <h2>Current Date</h2>
-                <h2>{this.state.datetime.toLocaleDateString()}</h2>
+                <h2 id='welcomeName'>Welcome to RiskManagement {this.props.user.username}!</h2>
+                <h3></h3>
+                <h2>Current Time: {this.state.datetime.toLocaleTimeString()}</h2>
+                <h2>Current Date: {this.state.datetime.toLocaleDateString()}</h2>
+                <h2>Path of Working Directory: {this.props.pathname}</h2>
                 {this.interval()}
                 <NavLink id ='logout' to='/login'>Logout</NavLink>
             </div>
