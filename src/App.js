33f4385475778b6e3d031657e0ab424cdaac0fde
github username <github email address>
2020-05-15 11:17:09 -0600

    Add login page and styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './Components/LoginPage'
+import PrivateRoute from './Components/PrivateRoute'
+import {Switch, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  state = {
+    user: [],
+    isLoggedIn: false,
+    isCorrectUser: true,
+    isIncorrectPassword: false,
+  }
+  
+  componentDidMount() {
+    console.log('mount')
+    if(localStorage.token){
+      fetch(`https://recipe-collector-capstone.herokuapp.com/users/authenticate`, {
+      method: "GET",
+      headers: {
+        'Authorization': `Bearer ${localStorage.token}`
+      }
+      })
+      .then(response => response.json())
+      .then(result => this.setState({user: result.user}))
+    }
+  }
+
+  login = (user) => {
+    fetch("https://recipe-collector-capstone.herokuapp.com/users/login", {
+       method: 'POST',
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify({username: user.username, password: user.password})
+     })
+     .then(response => response.json())
+     .then(response => {
+       if(response.status == 401)
+       { this.setState({isCorrectUser: false}) }
+      else {
+       localStorage.setItem('token', response.token)
+       localStorage.setItem('user_id', response.foundUser.id)
+       this.componentDidMount()
+       this.setState({isLoggedIn: true, user: response.foundUser})}
+      })
+   }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Switch> 
+            <PrivateRoute exact user={this.state.user} path='/' />
+            <Route path='/login' render={(props) => <LoginPage {...props} 
+               isLoggedIn={this.state.isLoggedIn} isCorrectUser={this.state.isCorrectUser} 
+               login={this.login} 
+              />}
+            /> 
+        <LoginPage/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+
